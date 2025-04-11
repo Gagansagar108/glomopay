838df60cb9c48282000a36629fc845ca837579a1
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2025-04-11 09:35:21 +0530

    init schema

--- db/migrate/20250411035941_create_multi_provider_schema.rb
+++ db/migrate/20250411035941_create_multi_provider_schema.rb
@@ -1,7 +1,33 @@
 class CreateMultiProviderSchema < ActiveRecord::Migration[8.0]
   def change
-    create_table :multi_provider_schemas do |t|
+    create_table :users do |t|
+      t.string :name
+      t.streing :email 
+      t.string :phone
+      t.string :country_code 
+      t.boolean :status
       t.timestamps
     end
+
+    add
+
+    create_table :notifications do |t|
+      t.references :user
+      t.string :message 
+      t.string :channel 
+      t.string :provider 
+      t.string :status 
+      t.timestamps
+    end 
+
+    create_table :notification_provider_configs do |t|
+      t.string :channel
+      t.string :name 
+      t.json :config_data 
+      t.boolean :status
+      t.timestamps
+    end 
+
+    add_index :notification_provider_configs, :channel
   end
 end
