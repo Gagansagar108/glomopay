d7b7ad26040be1b5d7245e021884990b92b67f56
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2025-04-11 09:38:01 +0530

    background job tracker

--- db/migrate/20250411035941_create_multi_provider_schema.rb
+++ db/migrate/20250411035941_create_multi_provider_schema.rb
@@ -2,7 +2,7 @@ class CreateMultiProviderSchema < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.streing :email 
+      t.string :email 
       t.string :phone
       t.string :country_code 
       t.boolean :status
@@ -25,6 +25,18 @@ class CreateMultiProviderSchema < ActiveRecord::Migration[8.0]
       t.string :name 
       t.json :config_data 
       t.boolean :status
+      t.timestamps
+    end 
+
+    create_table :background_job_tracker do |t|
+      t.string :job_id
+      t.string :worker_name 
+      t.integer :status 
+      t.json :arguments 
+      t.datetime :scheduled_at
+      t.datetime :completed_at
+      t.datetime :failed_at
+      t.string :failure_reason 
       t.timestamps
     end 
 
